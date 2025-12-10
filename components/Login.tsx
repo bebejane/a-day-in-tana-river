13781a97a50828901b6e6c3f485cc1c566486a60
Bebe Jane <bjornthief@gmail.com>
2025-12-10 20:03:23 +0100

    text password

--- components/Login.tsx
+++ components/Login.tsx
@@ -56,7 +56,7 @@ export default function Login({ intro }: LoginProps) {
 				<Content content={intro} className={s.intro} />
 				<form onSubmit={handleSubmit}>
 					<input
-						type='password'
+						type='text'
 						value={password}
 						placeholder='Please enter your password'
 						onChange={(e) => setPassword(e.target.value)}
