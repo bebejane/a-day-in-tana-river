ed1505bef2907b803b69783d3813d390f3c52ed1
Bebe Jane <bjornthief@gmail.com>
2025-12-10 16:55:58 +0100

    fix

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,9 +3,9 @@ import { cookies } from 'next/headers';
 export async function POST(req: Request) {
 	const cookiStore = await cookies();
 
-	if (cookiStore.get('user')?.value === 'authorized') {
-		return new Response('authorized', { status: 200 });
-	}
+	// if (cookiStore.get('user')?.value === 'authorized') {
+	// 	return new Response('authorized', { status: 200 });
+	// }
 	const { password } = await req.json();
 
 	if (password === process.env.STATIC_PASSWORD) {
